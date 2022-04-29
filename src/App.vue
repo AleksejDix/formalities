e8041c9bbbfd1617dfaee5d9fbc160d14517cdd7
Aleksej Dix <git@aleksejdix.com>
2022-04-29 10:24:00 +0200

    add formkit

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,46 @@
   <header>
     <div class="wrapper">nothing here</div>
     <HalloLib> world </HalloLib>
+
+    <FormKit type="group" v-model="formData">
+      <FormKit
+        name="hello_world"
+        label="Hello World"
+        placeholder="Enter something here..."
+        validation="required"
+        validation-behavior="live"
+        help="Read the docs for more info on using FormKit"
+      />
+      <FormKit
+        name="opinion"
+        label="Opinion"
+        help="How excited are you about FormKit?"
+        type="radio"
+        value="A lot"
+        :options="['A little', 'A lot']"
+      />
+      <div class="side-by-side">
+        <FormKit
+          name="recommendation"
+          label="Recommendation"
+          v-model="recommendation"
+          help="How likely are you to recommend FormKit to a friend?"
+          type="range"
+          min="0"
+          max="10"
+        />
+        <pre class="range-output">{{ recommendation }} </pre>
+      </div>
+    </FormKit>
   </header>
 
   <main></main>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import HalloLib from "./components/HalloLib.vue";
+
+const recommendation = ref("10");
+const formData = ref({});
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { plugin, defaultConfig } from "@formkit/vue";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(plugin, defaultConfig);
+
+app.mount("#app");
