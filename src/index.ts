d850730dc21518bbd5fb0900a703af56012a4fd2
Srdjan <srdjan.pajic@immosparrow.ch>
2022-06-08 17:35:56 +0200

    Install formkit and 1 component test

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
 import type { App } from 'vue'
+import { plugin, defaultConfig } from '@formkit/vue';
+import { generateClasses } from '@formkit/tailwindcss';
+import messages from './formkit.config';
+import { de, fr, en, it } from '@formkit/i18n';
+import pctTheme from './assets/property-captain-theme.js';
+
 export { default as pctFormkitTheme } from '@/assets/property-captain-theme';
 export { default as FormButton } from '@/components/FormButton.vue';
 import FormButton from '@/components/FormButton.vue'
 
 export function install(app: App) {
+  app.use(
+    plugin,
+    defaultConfig({
+      locales: { de, fr, it, en },
+      locale: 'en',
+      config: {
+        classes: generateClasses(pctTheme)
+      },
+      messages: messages
+    })
+  );
   app.component('FormButton', FormButton)
 }
