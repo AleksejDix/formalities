6a99be9dd1ebdf07d297007b2f08cc07074cd08c
Srdjan <srdjan.pajic@immosparrow.ch>
2022-06-08 17:06:00 +0200

    Export component test2

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
+import type { App } from 'vue'
 export { default as pctFormkitTheme } from '@/assets/property-captain-theme';
 export { default as FormButton } from '@/components/FormButton.vue';
+import FormButton from '@/components/FormButton.vue'
+
+export function install(app: App) {
+  app.component('FormButton', FormButton)
+}
